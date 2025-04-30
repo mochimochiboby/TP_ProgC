e78c93d80918a165804ea805fd4d48570c1c1298
mochimochiboby <lahaye809@gmail.com>
2025-04-30 16:23:31 +0200

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    unsigned int d = 0b10111011010101010010101010101010;  // Exemple de nombre binaire
-
-    
-    unsigned int mask4 = 1 << 3;  
-    unsigned int mask20 = 1 << 19; 
-  
-    int bit4 = (d & mask4) >> 3;  
-    int bit20 = (d & mask20) >> 19;
-
-    // Vérification des deux bits : si les deux sont à 1, afficher 1, sinon afficher 0
-    if (bit4 == 1 && bit20 == 1) {
+    int d = 268439552;
+    int pos4 = (d >> 28) & 1;
+    int pos20 = (d >> 12) & 1;
+    if (pos4 && pos20) {
         printf("1\n");
     } else {
         printf("0\n");
     }
-
     return 0;
 }
-
