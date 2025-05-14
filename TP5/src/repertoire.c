25b6c653cd14c0b664f64843eddd7167681f7c8c
mochimochiboby <lahaye809@gmail.com>
2025-05-14 17:15:15 +0200

    Update repertoire.c

--- TP5/src/repertoire.c
+++ TP5/src/repertoire.c
@@ -1,23 +1,35 @@
+// repertoire.c
+#include "repertoire.h"
 #include <stdio.h>
 #include <dirent.h>
-#include <string.h>
+#include <stdlib.h>
 
-void lire_dossier(const char *nom_repertoire) {
-    DIR *dir = opendir(nom_repertoire);
-    if (dir == NULL) {
-        perror("Erreur : impossible d'ouvrir le répertoire");
+// Fonction qui lit le contenu d'un répertoire
+void lire_dossier(const char nom_repertoire) {
+    DIRrep = opendir(nom_repertoire);
+
+    if (rep == NULL) {
+        perror("Erreur lors de l'ouverture du répertoire");
         return;
     }
 
-    struct dirent *ent;
-    printf("Contenu du répertoire \"%s\" :\n", nom_repertoire);
-
-    while ((ent = readdir(dir)) != NULL) {
-        // Ignorer les entrées "." et ".."
-        if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
-            printf(" - %s\n", ent->d_name);
-        }
+    struct dirent entree;
+    while ((entree = readdir(rep)) != NULL) {
+        printf("%s\n", entree->d_name);
     }
 
-    closedir(dir);
+    closedir(rep);
+}
+
+// Fonction main intégrée ici
+int main(int argc, charargv[]) {
+    if (argc != 2) {
+        printf("Utilisation : %s <nom_du_repertoire>\n", argv[0]);
+        return 1;
+    }
+
+    char *nom_repertoire = argv[1];
+    lire_dossier(nom_repertoire);
+
+    return 0;
 }
